f67f1b7a9cbe2afc3b37b6c632f919f425c50b44
大业 <boyisire@foxmail.com>
2018-03-05 14:32:21 +0800

    路由和逻辑测试代码

--- public/index.php
+++ public/index.php
@@ -1,7 +1,18 @@
 <?php
 
+use Slim\Http\Request;
+use Slim\Http\Response;
+
 require __DIR__.'/../bootstrap/autoload.php';
 
 $app = require_once __DIR__.'/../bootstrap/app.php';
+
+$app->get('/hello/{name}',function (Request $request,Response $response){
+    $name = $request->getAttribute('name');
+    $response->getBody()->write("Hello [".$name."]!  Welcome To DY-Fromework.");
+
+    return $response;
+});
+
 
 $app->run();
